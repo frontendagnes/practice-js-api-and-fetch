a15e3822dddb58da03fbd4e42eb3541def22012b
Agnieszka Kaminska <zabula81@o2.pl>
2024-01-19 11:34:13 +0100

    fifth

--- 05/app.js
+++ 05/app.js
@@ -1,37 +1,63 @@
-const apiUrl = 'http://localhost:3000/users';
+const apiUrl = "http://localhost:3000/users";
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    loadUsers();
+  const form = document.querySelector("form");
+  loadUsers();
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetchPost(apiUrl, form);
+  });
 }
 
 function loadUsers() {
-    const promise = fetchGet(apiUrl);
+  const promise = fetchGet(apiUrl);
 
-    promise
-        .then(data => insertUsers(data))
-        .catch(err => console.error(err));
+  promise.then((data) => insertUsers(data)).catch((err) => console.error(err));
 }
 
 function fetchGet(url) {
-    return fetch(url)
-        .then(resp => {
-            if(resp.ok) {
-                return resp.json();
-            }
+  return fetch(url).then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    }
 
-            return Promise.reject(resp);
-        });
+    return Promise.reject(resp);
+  });
 }
 
 function insertUsers(usersList) {
-    const ulElement = document.querySelector('.users');
-    ulElement.innerHTML = '';
-    usersList.forEach(user => {
-        const liElement = document.createElement('li');
-        liElement.innerText = `${user.firstName} ${user.lastName}`;
+  const ulElement = document.querySelector(".users");
+  ulElement.innerHTML = "";
+  usersList.forEach((user) => {
+    const liElement = document.createElement("li");
+    liElement.innerText = `${user.firstName} ${user.lastName}`;
 
-        ulElement.appendChild(liElement);
-    });
+    ulElement.appendChild(liElement);
+  });
+}
+
+function randomID() {
+  return Math.floor(Math.random() * (100 + 1));
+}
+
+function fetchPost(url, form) {
+  const { firstName, lastName } = form;
+  const data = {
+    id: randomID(),
+    firstName: firstName.value,
+    lastName: lastName.value,
+  };
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  loadUsers();
 }
